513071dbb3910db9915a50ea26cb98667d6bfdf3
Tym0x <tymon.pienkos@gmial.com>
2020-07-31 15:30:39 +0200

    AuthorPosts

---
+++ components/tymox/AuthorPosts.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="post" v-for= "post in posts"> 
+            <h1>{{post.title}}</h1>
+            <h4>{{post.created}}</h4>
+            <div>{{post.content}}</div>
+        </div>
+    </div>
+</template>
+
+<style>
+.post {
+    padding: 10px;
+    margin: 10px;
+    border: 1px dashed green;
+}
+</style>
+
+<script>
+export default {
+    props: {
+        potsToShow: {
+        type: Array,
+        required: true
+        }
+    }
+}
+</script>
