11fe11515014bf487527d2a082a1417836932511
Maciej Chmurski <contact@t2n.eu>
2020-07-31 15:55:50 +0200

    Update authors.vue

--- pages/authors.vue
+++ pages/authors.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <a href="/authors/stachu">stachu</a>
-    <a href="/authors/tymox">tymox</a>
-    <a href="/authors/dvisyy">dvisyy</a>
-    <a href="/authors/chmura">chmura</a>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <a href="/authors/stachu" class="btn btn-primary">stachu</a>
+      <a href="/authors/tymox" class="btn btn-primary">tymox</a>
+      <a href="/authors/dvisyy" class="btn btn-primary">dvisyy</a>
+      <a href="/authors/chmura" class="btn btn-primary">chmura</a>
+    </div>
     <nuxt-child />
   </div>
 </template>
