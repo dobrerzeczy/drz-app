f5f626921013e0aec2b94b15e5077baf2cb2906e
Maciej Chmurski <contact@t2n.eu>
2020-07-31 14:28:24 +0200

    authors router

--- pages/authors.vue
+++ pages/authors.vue
@@ -3,5 +3,6 @@
     <a href="/authors/stachu">stachu</a>
     <a href="/authors/tymox">tymox</a>
     <a href="/authors/dvisyy">dvisyy</a>
+    <nuxt-child />
   </div>
 </template>
---
+++ pages/authors/tymox.vue
@@ -0,0 +1,3 @@
+<template>
+    <div> cześć </div>
+</template>
