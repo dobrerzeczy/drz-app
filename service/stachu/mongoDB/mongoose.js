d0c8beb41734d84aab0cd9e08031b6e1c7a34dcd
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-07-31 15:02:10 +0200

    page and back-end

---
+++ service/stachu/mongoDB/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require(`mongoose`);
+const mongoURL = `mongodb://localhost:27017/PROJEKTT`
+
+
+mongoose.connect(mongoURL, {
+useCreateIndex: true,
+useNewUrlParser: true,
+useUnifiedTopology: true,
+}, (error, done) => {
+    if(error)
+    {
+        console.log(error);
+    }
+    else if(done)
+    {
+        console.log(`Polaczono z mongoDB!`);
+    }
+})
