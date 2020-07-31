cfec1fe38a22803f75f8570c9fe7da78f51591e4
Maciej Chmurski <contact@t2n.eu>
2020-07-31 15:02:28 +0200

    chmura page

--- pages/authors.vue
+++ pages/authors.vue
@@ -3,6 +3,7 @@
     <a href="/authors/stachu">stachu</a>
     <a href="/authors/tymox">tymox</a>
     <a href="/authors/dvisyy">dvisyy</a>
+    <a href="/authors/chmura">chmura</a>
     <nuxt-child />
   </div>
 </template>
--- pages/authors/tymox.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div> cześć </div>
-</template>
