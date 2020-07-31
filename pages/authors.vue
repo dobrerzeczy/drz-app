7ea6edc7d83fefe95c6311ffc147ccd7c0be0f33
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-07-31 15:56:24 +0200

    update

--- pages/authors.vue
+++ pages/authors.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <a href="/authors/stachu">stachu</a>
-    <a href="/authors/tymox">tymox</a>
-    <a href="/authors/dvisyy">dvisyy</a>
-    <a href="/authors/chmura">chmura</a>
+
+
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <a href="/authors/stachu" class="btn btn-secondary">stachu</a>
+      <a href="/authors/tymox" class="btn btn-secondary">tymox</a>
+      <a href="/authors/dvisyy" class="btn btn-secondary">dvisyy</a>
+      <a href="/authors/chmura" class="btn btn-secondary">chmura</a>
+    </div>
     <nuxt-child />
   </div>
 </template>
