89a341635a3aa0dfda5e4c4940198bb7e4a8d10b
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-08-17 14:55:32 +0200

    profile

--- pages/authors.vue
+++ pages/authors.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-    
+  <div class="container">
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <a href="/authors/stachu" class="btn btn-secondary">stachu</a>
+      <a href="/authors/tymox" class="btn btn-secondary">tymox</a>
+      <a href="/authors/dvisyy" class="btn btn-secondary">dvisyy</a>
+      <a href="/authors/chmura" class="btn btn-secondary">chmura</a>
+      <a href="/authors/jedrzej" class="btn btn-secondary">jedrzej</a>
+      <a href="/authors/jacek" class="btn btn-secondary">jacek</a>
+    </div>
 
-  <div class="btn-group" role="group" aria-label="Basic example">
-  <a href="/authors/stachu" class="btn btn-secondary">Stachu</a>
-  <a href="/authors/tymox" class="btn btn-secondary">tymox</a>
-  <a href="/authors/dvisyy" class="btn btn-secondary">dvissy</a>
-  <a href="/authors/chmura" class="btn btn-secondary">Chmura</a>
-  
-</div>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Library</li>
+      </ol>
+    </nav>
+
 
     <nuxt-child />
   </div>
 </template>
+
+<script>
+export default {
+
+}
+
+</script>
