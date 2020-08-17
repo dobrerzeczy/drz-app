4eaab8513918bbb72cf5e41c79cf685c123f90fe
Tym0x <tymon.pienkos@gmial.com>
2020-08-17 12:16:05 +0200

    zmiany

--- pages/authors.vue
+++ pages/authors.vue
@@ -3,8 +3,8 @@
     
 
   <div class="btn-group" role="group" aria-label="Basic example">
-  <a href="/authors/stachu" class="btn btn-secondary">tymox</a>
-  <a href="/authors/tymox" class="btn btn-secondary">Stachu</a>
+  <a href="/authors/stachu" class="btn btn-secondary">Stachu</a>
+  <a href="/authors/tymox" class="btn btn-secondary">tymox</a>
   <a href="/authors/dvisyy" class="btn btn-secondary">dvissy</a>
   <a href="/authors/chmura" class="btn btn-secondary">Chmura</a>
   
